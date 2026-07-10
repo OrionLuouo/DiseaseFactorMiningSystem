c6ee945bcfb6e63cae6bc49dccbd93abd3790d93
18136 <1813681422@qq.com>
2026-07-10 20:06:12 +0800

    初期项目架构

--- Web/app.py
+++ Web/app.py
@@ -1,3 +1,15 @@
-from flask import Flask , render_template
+from flask import app, render_template, Flask
 
-@app.route
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+def initial():
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    initial()
+else:
+    print("Not running as main program")
